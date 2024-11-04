2aa8a83fc84d3c13d62ebc07eafd28bac44310f0
#C47 <53994242+FENGMOPUKE@users.noreply.github.com>
2024-11-03 21:40:13 -0500

    Update Coding.py

--- Coding.py
+++ Coding.py
@@ -3,7 +3,6 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plot
 import plotly.graph_objects as go
-import yfinance as yf
 
 # 1&2
 data = yf.download("^GSPC ^IXIC GLD ^VIX", start="2007-01-01", end="2020-03-31")["Close"]
@@ -22,11 +21,13 @@ stocks = {'GSPC': GSPC,
 stocks_bear_market = {}
 stocks_bull_market = {}
 stocks_covid_market = {}
+
 def analyze_period(period_name, stock, start_date, end_date):
     df1 = stock[start_date: end_date]
     df1_returns = df1['Return']
     df1_cum_returns = df1['Cum Return']
     return df1
+
 for stock in stocks.keys():
     stocks[stock]['Return'] = stocks[stock]['Close'].pct_change()
     stocks[stock]['Cum Return'] = (1 + stocks[stock]['Return']).cumprod()
@@ -34,30 +35,67 @@ for stock in stocks.keys():
     stocks_bull_market[stock] = analyze_period('bull', stocks[stock], bull_market_start, bull_market_end)
     stocks_covid_market[stock] = analyze_period('covid', stocks[stock], covid_crisis_start, covid_crisis_end)
 
+
 # 3
 stock_cum_return = pd.DataFrame()
 for stock in stocks.keys():
     stock_cum_return[stock] = stocks[stock]['Cum Return']
-stock_cum_return
+
 def plot_compare(dict1):
     for key in dict1.keys():
         dict1[key]['Cum Return'].plot(figsize = (15,6))
     plot.legend(dict1.keys())
-    #data = pd.concat([df['Cum Return'] for df in dict1.values()], axis=1)
-    #data.plot(figsize=(10,5), legend = dict1.keys())
+
 def plot_compare2(fig, dict1, period_name):
     for key in dict1.keys():
         fig.add_trace(go.Scatter(x=dict1[key].index, y=dict1[key]['Cum Return'], mode='lines', name=key))
     fig.update_layout(
-    title={'text':'Cumulative Performance of the ' + period_name},
-    title_x=0.5, 
-    xaxis=dict(
-        title='Date',
-    ),
-    yaxis=dict(
-        title='Return',
-         ticksuffix="%"
-    ),
-    paper_bgcolor='rgba(0,0,0,0)',
-    plot_bgcolor='rgba(0,0,0,0)',
-    )
+              title={'text':'Cumulative Performance of the ' + period_name},
+              title_x=0.5, 
+              xaxis=dict(
+                  title='Date',
+                        ),
+              yaxis=dict(
+                  title='Return',
+                   ticksuffix="%"
+              ),
+              paper_bgcolor='rgba(0,0,0,0)',
+              plot_bgcolor='rgba(0,0,0,0)')
+     return fig
+
+
+# 4
+fig = go.Figure()
+plot_compare2(fig, stocks, 'Entire Period')
+fig
+
+fig = go.Figure()
+plot_compare2(fig, stocks_bear_market, 'Bear Market')
+fig
+
+fig = go.Figure()
+plot_compare2(fig, stocks_bull_market, 'Bull Market')
+fig
+
+fig = go.Figure()
+plot_compare2(fig, stocks_covid_market, 'Covid Period')
+fig
+
+stock_return = pd.DataFrame()
+stock_return['GSPC'] = data['^GSPC'].pct_change()
+stock_return['IXIC'] = data['^IXIC'].pct_change()
+stock_return['GLD'] = data['GLD'].pct_change()
+stock_return['VIX'] = data['^VIX'].pct_change()
+stock_return
+
+
+#5
+def df_subset(df, start, point1, point2, end):
+    df1 = df[start : point1]
+    df2 = df[point1 : point2]
+    df3 = df[point2 : end]
+    return pd.DataFrame(df1), pd.DataFrame(df2), pd.DataFrame(df3)
+GSPC_bear, GSPC_bull, GSPC_covid = df_subset(stock_return['GSPC'], "2007-01-01", "2009-03-31", "2020-01-31", "2020-03-31")
+GLD_bear, GLD_bull, GLD_covid = df_subset(stock_return['GLD'], "2007-01-01", "2009-03-31", "2020-01-31", "2020-03-31")
+IXIC_bear, IXIC_bull, IXIC_covid = df_subset(stock_return['IXIC'], "2007-01-01", "2009-03-31", "2020-01-31", "2020-03-31")
+VIX_bear, VIX_bull, VIX_covid = df_subset(stock_return['VIX'], "2007-01-01", "2009-03-31", "2020-01-31", "2020-03-31")
