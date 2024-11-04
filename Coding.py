6d3b7e1dccc26bd0fdcf67bfa23350b97dc6fe41
#C47 <53994242+FENGMOPUKE@users.noreply.github.com>
2024-11-03 22:37:36 -0500

    Update Coding.py

--- Coding.py
+++ Coding.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plot
 import plotly.graph_objects as go
+import statsmodels.api as sm
 
 # 1&2
 data = yf.download("^GSPC ^IXIC GLD ^VIX", start="2007-01-01", end="2020-03-31")["Close"]
@@ -50,43 +51,39 @@ def plot_compare2(fig, dict1, period_name):
     for key in dict1.keys():
         fig.add_trace(go.Scatter(x=dict1[key].index, y=dict1[key]['Cum Return'], mode='lines', name=key))
     fig.update_layout(
-              title={'text':'Cumulative Performance of the ' + period_name},
-              title_x=0.5, 
-              xaxis=dict(
-                  title='Date',
-                        ),
-              yaxis=dict(
-                  title='Return',
-                   ticksuffix="%"
-              ),
-              paper_bgcolor='rgba(0,0,0,0)',
-              plot_bgcolor='rgba(0,0,0,0)')
-     return fig
+        title={'text':'Cumulative Performance of the ' + period_name},
+        title_x=0.5, 
+        xaxis=dict(
+            title='Date',
+            ),
+        yaxis=dict(
+            title='Return',
+            ticksuffix="%"
+            ),
+        paper_bgcolor='rgba(0,0,0,0)',
+        plot_bgcolor='rgba(0,0,0,0)'
+    )
+    return fig
 
 
 # 4
-fig = go.Figure()
-plot_compare2(fig, stocks, 'Entire Period')
-fig
+fig1 = plot_compare2(go.Figure(), stocks, 'Entire Period')
+fig1.show()
 
-fig = go.Figure()
-plot_compare2(fig, stocks_bear_market, 'Bear Market')
-fig
+fig2 = plot_compare2(go.Figure(), stocks_bear_market, 'Bear Market')
+fig2.show()
 
-fig = go.Figure()
-plot_compare2(fig, stocks_bull_market, 'Bull Market')
-fig
+fig3 = plot_compare2(go.Figure(), stocks_bull_market, 'Bull Market')
+fig3.show()
 
-fig = go.Figure()
-plot_compare2(fig, stocks_covid_market, 'Covid Period')
-fig
+fig4 = plot_compare2(go.Figure(), stocks_covid_market, 'Covid Period')
+fig4.show()
 
 stock_return = pd.DataFrame()
 stock_return['GSPC'] = data['^GSPC'].pct_change()
 stock_return['IXIC'] = data['^IXIC'].pct_change()
 stock_return['GLD'] = data['GLD'].pct_change()
 stock_return['VIX'] = data['^VIX'].pct_change()
-stock_return
 
 
 #5
@@ -95,7 +92,30 @@ def df_subset(df, start, point1, point2, end):
     df2 = df[point1 : point2]
     df3 = df[point2 : end]
     return pd.DataFrame(df1), pd.DataFrame(df2), pd.DataFrame(df3)
+
 GSPC_bear, GSPC_bull, GSPC_covid = df_subset(stock_return['GSPC'], "2007-01-01", "2009-03-31", "2020-01-31", "2020-03-31")
 GLD_bear, GLD_bull, GLD_covid = df_subset(stock_return['GLD'], "2007-01-01", "2009-03-31", "2020-01-31", "2020-03-31")
 IXIC_bear, IXIC_bull, IXIC_covid = df_subset(stock_return['IXIC'], "2007-01-01", "2009-03-31", "2020-01-31", "2020-03-31")
 VIX_bear, VIX_bull, VIX_covid = df_subset(stock_return['VIX'], "2007-01-01", "2009-03-31", "2020-01-31", "2020-03-31")
+
+bear_correlation = pd.concat([GSPC_bear, GLD_bear, IXIC_bear, VIX_bear], axis = 1).corr()
+print(bear_correlation)
+
+bull_correlation = pd.concat([GSPC_bull, GLD_bull, IXIC_bull, VIX_bull], axis = 1).corr()
+print(bull_correlation)
+
+covid_correlation = pd.concat([GSPC_covid, GLD_covid, IXIC_covid, VIX_covid], axis = 1).corr()
+print(covid_correlation)
+
+def beta_cal(Y_list, X):
+    beta_list = []
+    for Y in Y_list:
+        model = sm.OLS(Y,X,missing="drop").fit()
+        beta_list.append(model.params['GSPC'])
+    return beta_list
+
+beta_bear = beta_cal([GLD_bear, VIX_bear], GSPC_bear)
+beta_bull = beta_cal([GLD_bull, VIX_bull], GSPC_bull)
+beta_covid = beta_cal([GLD_covid, VIX_covid], GSPC_covid)
+beta_list = [beta_bear, beta_bear, beta_bear]
+print(beta_list)
